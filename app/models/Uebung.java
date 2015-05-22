7cfe68ec23e72967170432f9916b223ef7c3bcc5
ArifC <cerit_arif@gmx.de>
2015-05-22 17:50:53 +0200

    mysql anbindung

---
+++ app/models/Uebung.java
@@ -0,0 +1,35 @@
+package models;
+
+
+import play.data.format.*;
+import play.db.ebean.*;
+import javax.persistence.*;
+import play.data.validation.*;
+import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+@Entity
+public class Uebung {
+    @Id
+    public int uebung_id;
+    @Constraints.Required
+    private String name;
+    @Constraints.Required
+    private List<Muskel> muskeln = new ArrayList<Muskel>();
+    @Constraints.Required
+    private String beschreibung;
+    @Constraints.Required
+    private String schwierigkeit;
+
+    public Uebung(String name, Muskel muskel, String beschreibung, String schwierigkeit){
+        this.name=name;
+        this.muskeln.add(muskel);
+        this.beschreibung = beschreibung;
+        this.schwierigkeit=schwierigkeit;
+    }
+
+
+}
