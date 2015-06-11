a0f681da90002b06e49ef74e40b3f94e1ff3fa9a
arifC <cerit_arif@gmx.de>
2015-06-11 10:12:47 +0200

    Bmirechner und Kalorienrechner

--- public/javascripts/help_bmi.js
+++ public/javascripts/help_bmi.js
@@ -1,44 +1,47 @@
-function bmirechnen(){
+function calcBMI(){
     var gewicht = document.getElementById("gewichtB").value;
     var groesse = document.getElementById("groesseB").value;
-    var geschlecht = document.getElementById("gesch").options[document.getElementById("gesch").selectedIndex].text
+    var e = document.getElementById("geschlecht");
+    var strGeschlecht = e.options[e.selectedIndex].text;
+
     if(groesse > 2.5 || gewicht < 30 || gewicht > 300){
-        alert("Falsche Eingabe")
+        alert("Falsche Eingabe");
     }
-    var bmi = gewicht/(groesse*groesse);
-    if(geschlecht == "männlich"){
-        if(bmi<20){
-            $("#bmierg").html("Untergewichtig der BMI liegt bei:" +bmi);
-        }
-        if (bmi>=20 && bmi<25){
-            $("#bmierg").html("Normalgewichtig der BMI liegt bei:"+bmi);
-        }
-        if(bmi>=25 && bmi<20){
-            $("#bmierg").html("Leichtes Übergewicht der BMI liegt bei:" +bmi);
-        }
-        if(bmi>=30 && bmi<40){
-            $("#bmierg").html("Übergewichtig der BMI liegt bei:"+bmi);
-        }
-        if(bmi>=40){
-            $("#bmierg").html("Sehr starkes Übergewicht der BMI liegt bei:"+bmi);
-        }
+    else{
+        var bmi = gewicht/(groesse*groesse);
+        if(strGeschlecht == "männlich"){
+            if(bmi<20){
+                $("#bmierg").html("Untergewichtig der BMI liegt bei:" +bmi);
+            }
+            if (bmi>=20 && bmi<26){
+                $("#bmierg").html("Normalgewichtig der BMI liegt bei:"+bmi);
+            }
+            if(bmi>=26 && bmi<30){
+                $("#bmierg").html("Leichtes Übergewicht der BMI liegt bei:" +bmi);
+            }
+            if(bmi>=30 && bmi<40){
+                $("#bmierg").html("Übergewichtig der BMI liegt bei:"+bmi);
+            }
+            if(bmi>=40){
+                $("#bmierg").html("Sehr starkes Übergewicht der BMI liegt bei:"+bmi);
+            }
+        }else{
+            if(bmi<19){
+                $("#bmierg").html("Untergewichtig der BMI liegt bei:"+bmi);
+            }
+            if(bmi>=19 && bmi<24){
+                $("#bmierg").html("Normalgewichtig der BMI liegt bei:"+bmi);
+            }
+            if(bmi>=24 && bmi<30){
+                $("#bmierg").html("Leichtes Übergewicht der BMI liegt bei:"+bmi);
+            }
+            if(bmi>=30 && bmi<40){
+                $("#bmierg").html("Übergewichtig der BMI liegt bei:"+bmi);
+            }
+            if(bmi>=40){
+                $("#bmierg").html("Sehr starkes Übergewicht der BMI liegt bei:"+bmi);
+            }
 
-    }else{
-        if(bmi<19){
-            $("#bmierg").html("Untergewichtig der BMI liegt bei:"+bmi);
         }
-        if (bmi>=19 && bmi<24){
-            $("#bmierg").html("Normalgewichtig der BMI liegt bei:"+bmi);
-        }
-        if(bmi>=24 && bmi<30){
-            $("#bmierg").html("Leichtes Übergewicht der BMI liegt bei:"+bmi);
-        }
-        if(bmi>=30 && bmi<40){
-            $("#bmierg").html("Übergewichtig der BMI liegt bei:"+bmi);
-        }
-        if(bmi>=40){
-            $("#bmierg").html("Sehr starkes Übergewicht der BMI liegt bei:"+bmi);
-        }
-
     }
 }
--- public/javascripts/help_kalrechner.js
+++ public/javascripts/help_kalrechner.js
@@ -1,11 +1,11 @@
-function berechneKal(){
+function calcCal(){
     var gewicht = document.getElementById("gewichtK").value;
     var groesse = document.getElementById("groesseK").value;
     var alter = document.getElementById("alter").value;
     var selecter = document.getElementById("faktor");
     var faktor = selecter.options[selecter.selectedIndex].value;
 
-    var bedarf = 66.47 + 13.7*gewicht + 5*groesse -6.8*alter;
+    var bedarf = 66.47 + 13.7*gewicht + 500*groesse -6.8*alter;
     bedarf= bedarf*faktor;
-    $("#kalerg").html("Der Bedarf ist:" + bedarf);
+    $("#kalerg").html("Du brauchst am Tag: " + bedarf.toPrecision(4) + " Kalorien.");
 }
